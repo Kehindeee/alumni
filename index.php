5068b43de88186064e22d7145b9ad446c1e4c789
Meena Chand <meena.hgmd@gmail.com>
2024-03-06 06:16:06 -0800

    Contact Api

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ a.jqte_tool_label.unselectable {
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=home">Home</a></li>
 						<li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=alumni_list">Association</a></li>
-						<li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=association2">Programs & Events</a></li>
+						<li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=programs">Programs & Events</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=alumni_list">Alumni</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=gallery">Gallery</a></li>
                         <?php if(isset($_SESSION['login_id'])): ?>
